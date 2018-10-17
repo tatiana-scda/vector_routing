52446d5646a30e10ea498ea68feec534b2726aa5
tatiana <tatiana.sca@gmail.com>
2018-10-17 19:22:32 -0300

    initializing basic functions

---
+++ router.py
@@ -0,0 +1,28 @@
+import sys, json, random, socket
+
+def comandoAdd(self):
+    pass
+
+def comandoDel(self)
+    pass
+
+def comandoTrace(self):
+    pass
+
+def lerEntrada(self):
+    # leitura da linha do terminal, apos isso, define-se qual comando será executado seguindo o começo deste comando.
+    comando = sys.stdin.readline()
+    funcao  = comando.split() 
+    
+    if cmd.startswith('add'):
+        comandoAdd(self)
+    elif cmd.startswith('del'):
+        comandoDel(self)
+    elif cmd.startswith('trace'):
+        comandoTrace(self)
+
+def main():
+    pass
+
+if name == '__main__':
+    main()
