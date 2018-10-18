503946b3e2f083d9d9340338aa49a9b07a7e6393
Luísa Ribeiro <luisarbezerra@hotmail.com>
2018-10-18 00:43:23 -0300

    teste

--- router.py
+++ router.py
@@ -1,28 +1,32 @@
 import sys, json, random, socket
 
-def comandoAdd(self):
+def comandoAdd(comando):
+    funcao, vizinho, peso = comando.split()
+    print(funcao, vizinho, peso)
+
+def comandoDel(comando):
+    funcao, vizinho = comando.split()
     pass
 
-def comandoDel(self)
+def comandoTrace(comando):
+    funcao, destino = comando.split()
     pass
 
-def comandoTrace(self):
-    pass
-
-def lerEntrada(self):
+def lerEntrada():
     # leitura da linha do terminal, apos isso, define-se qual comando será executado seguindo o começo deste comando.
     comando = sys.stdin.readline()
-    funcao  = comando.split() 
-    
-    if cmd.startswith('add'):
-        comandoAdd(self)
-    elif cmd.startswith('del'):
-        comandoDel(self)
-    elif cmd.startswith('trace'):
-        comandoTrace(self)
+    funcao  = comando.split()[0]
+
+    # de acordo com o comando, direciona para a função do comando.
+    if funcao   == 'add':
+        comandoAdd(comando)
+    elif funcao == 'del':
+        comandoDel(comando)
+    elif funcao == 'trace':
+        comandoTrace(comando)
 
 def main():
-    pass
+    lerEntrada()
 
-if name == '__main__':
+if __name__ == '__main__':
     main()
